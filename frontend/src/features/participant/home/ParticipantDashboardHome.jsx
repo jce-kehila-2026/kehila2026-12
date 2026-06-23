889a20645f3f3df423aed83c87a8f6a13dbcb934
demadabbagh <demadabbagh8@gmail.com>
2026-06-24 00:48:44 +0300

    Refine participant dashboard appointment and empty states

--- frontend/src/features/participant/home/ParticipantDashboardHome.jsx
+++ frontend/src/features/participant/home/ParticipantDashboardHome.jsx
@@ -542,7 +542,7 @@ function AppointmentEmptyCard({ onBook }) {
             {t('bookAppointmentText')}
           </p>
           <div className="pd-feature__footer">
-            <PremiumCta variant="pink" onClick={onBook}>
+            <PremiumCta variant="soft" onClick={onBook}>
               {t('bookAppointment')}
             </PremiumCta>
           </div>
@@ -568,7 +568,7 @@ function EventEmptyCard({ onExplore }) {
             {t('exploreEventsText')}
           </p>
           <div className="pd-feature__footer">
-            <PremiumCta variant="pink" onClick={onExplore}>
+            <PremiumCta variant="soft" onClick={onExplore}>
               {t('exploreEvents')}
             </PremiumCta>
           </div>
